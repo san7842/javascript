ebf1f7b57ca3525f4ed6d0bebf4af449ce525321
san7842 <sandeepjjjjj536@gmail.com>
2025-01-03 23:52:57 +0530

    add

--- date.js
+++ date.js
@@ -10,13 +10,54 @@
 // console.log(ans4);
 // console.log(ans5);
 // console.log(ans6);
-let date=()=>{
-    // let show=document.querySelector("#dis")
-    //  show.innerHTML=new Date();
-    // let ans=new Date()
-    // let show=document.querySelector("#dis")
-    //  show.innerHTML=ans;
-    let days=[Sun ,Mon ,Tue ,Wed ,Thu ,Fri , Sat]
-    let show=document.querySelector("#dis")
+// let date=()=>{
+//     // let show=document.querySelector("#dis")
+//     //  show.innerHTML=new Date();
+//     // let ans=new Date()
+//     // let show=document.querySelector("#dis")
+//     //  show.innerHTML=ans;
+//     // let ans=new Date()
+//     // let show=document.querySelector("#dis")
+//     //  show.innerHTML=ans.getDay();
+//     let ans=new Date()
+//     let show=document.querySelector("#dis")
+//     let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//     show.innerHTML=days[ans.getDay()]
     
+// }
+// let age=()=>{
+//     let agee=document.querySelector("#yourdate").value ;
+//     let birthdate=new Date(agee);
+//     let today=new Date();
+//     let age=today.getFullYear()-birthdate.getFullYear();
+//     // let month=today.getMonth()-birthdate.getMonth();
+//     document.getElementById("age").innerText = "You age " + age ;
+
+
+// }
+// setInterval(()=>{
+//     let show=document.querySelector("#display")
+//     let time=new Date();
+//     show.innerHTML=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+// },1000)
+let alaram=()=>{
+    let show=document.querySelector("#display")
+    let inp=document.querySelector("#inp" ).value ;
+  let inter=  setInterval(()=>{
+        let time=new Date();
+        let alaramtime=`${time.getHours().toString().padStart(2,"0")}:${time.getMinutes.toString().padStart(2,"0")}` 
+
+        if(alaramtime==inp){
+            show.innerHTML="alarm  is ringing"
+            document.querySelector("#aud").paly()
+
+            setTimeout(()=>{
+                document.querySelector("#aud").pause()
+                clearInterval(inter)
+            },7000)
+        }
+        else{
+            show.innerHTML="Alaram set"
+        }
+    })
 }
