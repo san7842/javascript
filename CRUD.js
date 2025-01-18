f1d004cda3eb421be6ab8f971394068a670225e9
san7842 <sandeepjjjjj536@gmail.com>
2025-01-18 22:56:27 +0530

    add

--- CRUD.js
+++ CRUD.js
@@ -5,5 +5,42 @@ let fetchDate=async()=>{
     let res=await fetch(url,{method:"GET"})
     let date=await res.json()
     console.log(date);
+    let show=document.querySelector("#display")
+    date.map((e)=>{
+        show.innerHTML+=`
+        <tr>
+            <td>${e.name}</td>
+            <td>${e.age}</td>
+            <td>${e.number}</td>
+            <td>${e.problem}</td>
+            <td>${e.date}</td>
+            <td>${e.price}</td>
+            <td  style="color:red" onclick="deletee ('${e.id}')">Delete</td>
+        </tr>
+    `})
    
+}
+let deletee=(id)=>{
+    let url=`http://localhost:3000/doctor/${id}`
+    fetch(url,{method:"DELETE"})
+}
+let insertt=()=>{
+    let inpname=document.querySelector("#inpname").value;
+    let inpage=document.querySelector("#inpage").value;
+    let inpnum=document.querySelector("#inpnum").value;
+    let inpdate=document.querySelector("#inpdate").value;
+    let inp=document.querySelector("#inpprob").value;
+    let url='http://localhost:3000/doctor'
+    fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type":"appliction/json"
+        },
+        body:JSON.stringify({
+            name:inpname,
+            age:inpage,
+            
+        })
+    })
+
 }
